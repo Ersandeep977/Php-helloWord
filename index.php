2042913d3e67c31bbf3150f45f7c9949ada66682
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-11-01 12:48:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
-</body>
-</html>
+<!DOCTYPE>  
+<html>  
+    <body>  
+        <?php  
+            echo "Hello world using echo </br>";  
+            ECHO "Hello world using ECHO </br>";  
+            EcHo "Hello world using EcHo </br>";  
+        ?>  
+    </body>  
+</html> 
