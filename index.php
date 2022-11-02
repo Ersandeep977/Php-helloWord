2556c95120862a00483a9d3beac6a6e218801217
Sandeep Kumar Patel <78929192+Ersandeep977@users.noreply.github.com>
2022-11-02 10:51:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 <html>
 <body>
 
+<h1>My first PHP page</h1>
+
 <?php
-$color = "red";
-echo "My car is " . $color . "<br>";
-echo "My house is " . $COLOR . "<br>";
-echo "My boat is " . $coLOR . "<br>";
+echo "Hello World!";
 ?>
 
 </body>
